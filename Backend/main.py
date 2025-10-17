c0cf0bc87204c3feee9c1660478922341235ebda
codx_venu <vasulallu09@gmail.com>
2025-10-17 19:05:03 +0530

    deploy

--- Backend/main.py
+++ Backend/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
 #     allow_headers=["*"],
 # )
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.on_event("startup")
