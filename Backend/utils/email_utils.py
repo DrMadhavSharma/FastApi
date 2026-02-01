c6ad3fc80fdaa8a257cd079332cfb72103e383a0
MADHAV SHARMA <madhavsharma8194@gmail.com>
2026-02-02 00:06:27 +0530

    Update email_utils.py

--- Backend/utils/email_utils.py
+++ Backend/utils/email_utils.py
@@ -1,3 +1,12 @@
+import os
+
+import base64
+
+import sib_api_v3_sdk
+
+from sib_api_v3_sdk.rest import ApiException
+
+BREVO_API_KEY = os.getenv("BREVO_API_KEY", "").strip()
 def send_email(to_address, subject, message, content="html", attachment_file=None):
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key['api-key'] = BREVO_API_KEY
@@ -42,4 +51,5 @@ def send_email(to_address, subject, message, content="html", attachment_file=Non
         print("Brevo reason:", e.reason)
         print("Brevo body:", e.body)
         raise
+
 
